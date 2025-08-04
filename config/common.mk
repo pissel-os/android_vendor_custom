e5a3674cf590571444f82a2aa7b49e6d05a825f5
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-04 21:09:27 +0530

    config: Extend eppe check for CUSTOM_BUILD
    Change-Id: I6cc7d1ae2aed7b3ef2e2b9d838cbdaca8e40dd73

--- config/common.mk
+++ config/common.mk
@@ -6,5 +6,11 @@ include vendor/custom/config/bootanimation.mk
 # GMS
 include vendor/custom/config/pixel.mk
 
+# EPPE
+ifneq ($(TARGET_DISABLE_EPPE),true)
+# Require all requested packages to exist
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CUSTOM_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
+endif
+
 # version
 include vendor/custom/config/version.mk
