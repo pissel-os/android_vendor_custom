ff6ff4f90aaec591529c1f45d943ed3e2713cd05
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-05 13:49:37 +0530

    config: Setup pixel repos
    Change-Id: I8dabeb316aba6dcfe3da87bc40944e006862841e

--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,5 +1,8 @@
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_mini_tablet.mk)
 
+# Define tablet-specific variables
+TARGET_IS_TABLET := true
+
 # Inherit some common PixelOS stuff.
 $(call inherit-product, vendor/custom/config/common.mk)
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,5 +1,9 @@
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
 
+# Define tablet-specific variables
+TARGET_IS_TABLET := true
+WITH_GMS_COMMS_SUITE := false
+
 # Inherit some common PixelOS stuff.
 $(call inherit-product, vendor/custom/config/common.mk)
